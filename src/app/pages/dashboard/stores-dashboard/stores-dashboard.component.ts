647543d23704a38c6840f5ddaf911826017a7b74
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-05-11 19:49:13 -0500

    Feat: Creating new dashboard component
    Adding stores component to dashboard

---
+++ src/app/pages/dashboard/stores-dashboard/stores-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stores-dashboard',
+  imports: [],
+  templateUrl: './stores-dashboard.component.html',
+  styleUrl: './stores-dashboard.component.css'
+})
+export class StoresDashboardComponent {
+
+}
