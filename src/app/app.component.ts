4b808116446c766aa712c26d77d6aac4d132c649
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-05-12 22:34:42 -0500

    Fix: Solving issues with dynamic colors theme
    Some html components now have a dynamic style change with the dark or light theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,12 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.themeService.getDarkMode().subscribe(isDark => {
       console.log('AppComponent detectó isDarkMode:', isDark);
-      document.documentElement.classList.toggle('dark', isDark);
+
+      const html = document.documentElement;
+  
+      // Alterna clase "dark" (si la usas tú) y el atributo de Bootstrap
+      html.classList.toggle('dark', isDark);
+      html.setAttribute('data-bs-theme', isDark ? 'dark' : 'light');
     });
 
     this.themeService.getSection('pageContent').subscribe(colors => {
