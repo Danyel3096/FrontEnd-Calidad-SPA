a7e72e58070614b23e71f0adca174a27e7046de1
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-05-14 21:47:18 -0500

    Feat: Deleting products page

--- src/app/pages/products/product-detail.route.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export default [
-  {
-    path: ':id',
-    loadComponent: () => import('./product-detail/product-detail.component'),
-  },
-] as Routes;
